381890897d1c8af30ce3da8cb11f4791c7deb2bf
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-13 15:07:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+let namefield=document.querySelector("#name")
+let yearfield =document.querySelector("#year")
+let btn=document.querySelector("#button")
+btn.addEventListener("click",fullurl)
+let url=document.querySelector("#url")
+
+function fullurl() {
+	let baseurl="https://localhost:8080/";
+	if(namefield.value && yearfield.value){
+	 url.innerText=baseurl +"?"+namefield.id+"="+namefield.value+"&"+yearfield.id+"="+yearfield.value
+	}
+	if(namefield.value){
+		url.innerText=baseurl +"?"+namefield.id+"="+namefield.value
+}
+	else if(yearfield.value){
+        url.innerText = baseurl + "?" + yearfield.id + "=" + yearfield.value;
+    }
+}
+
+
